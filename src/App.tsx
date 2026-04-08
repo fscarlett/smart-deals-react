029f0df227ee6f79206ceffb70d9cec2732cfb9b
Fox Scarlett <foxontheone@gmail.com>
2026-04-07 22:50:11 -0700

    layout, header, main styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id='center'>
-        <div>
-          <h1>IQ Smart Deals</h1>
-          <p></p>
-        </div>
-      </section>
-
-      <section id='spacer'></section>
+      <main>
+        <section>hero</section>
+        <section>deals</section>
+        <section>categories</section>
+        <section>for you</section>
+      </main>
     </>
   )
 }
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>IQ Smart Deals</h1>
+      <div id='nav'>nav here</div>
+    </header>
+  )
+}
+export default Header
