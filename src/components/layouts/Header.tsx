3e590aa904e23cff25c5a241fdcea9adc247aec9
Fox Scarlett <foxontheone@gmail.com>
2026-04-08 00:01:43 -0700

    noto sans, header styles, main styles

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,6 +1,8 @@
+import styles from '../../styles/Header.module.css'
+
 function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>IQ Smart Deals</h1>
       <div id='nav'>nav here</div>
     </header>
