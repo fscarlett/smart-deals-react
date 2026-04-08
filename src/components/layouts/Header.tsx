476efabac9e70a46a059186f5d65d7f51603ffc8
Fox Scarlett <foxontheone@gmail.com>
2026-04-08 16:28:18 -0700

    structure header html

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,10 +2,30 @@ import styles from '../../styles/Header.module.css'
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <h1>IQ Smart Deals</h1>
-      <div id='nav'>nav here</div>
-    </header>
+    <nav className={styles.header}>
+      <div className='topbar'>
+        <div>
+          <div className='topbar-container'>
+            <div className='logo'>logo here</div>
+            <div className='login'>login button</div>
+            <div className='burger'>burger menu</div>
+          </div>
+        </div>
+      </div>
+      <div className='cat-nav'>
+        <div>
+          <div className='cat-nav-container'>
+            <div className='link-wrapper'>
+              <div className='cat-nav-item'>cat 1</div>
+              <div className='cat-nav-item'>cat 2</div>
+              <div className='cat-nav-item'>cat 3</div>
+              <div className='cat-nav-item'>cat 4</div>
+              <div className='cat-nav-item'>SEARCH</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 export default Header
