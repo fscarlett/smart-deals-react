64f18d7da4043036f1a61c9715e959bc65ec715a
Fox Scarlett <foxontheone@gmail.com>
2026-04-08 17:36:12 -0700

    install tailwind, header classes

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,20 +2,24 @@ import styles from '../../styles/Header.module.css'
 
 function Header() {
   return (
-    <nav className={styles.header}>
-      <div className='topbar'>
-        <div>
-          <div className='topbar-container'>
-            <div className='logo'>logo here</div>
-            <div className='login'>login button</div>
-            <div className='burger'>burger menu</div>
+    <nav className={`${styles.header} sticky top-0 w-full z-40 flex flex-col`}>
+      <div className='topbar w-full border-b border-gray-300'>
+        <div className='h-full w-full mx-auto flex items-center justify-center'>
+          <div className='topbar-container flex flex-row justify-between items-center h-full w-full max-w-[1500px] py-3'>
+            <div className='logo flex items-center'>
+              <a href='/'>logo here</a>
+            </div>
+            <div className='items-center flex '>
+              <div className='login px-2 py-3'>login button</div>
+              <div className='burger py-3'>burger menu</div>
+            </div>
           </div>
         </div>
       </div>
-      <div className='cat-nav'>
-        <div>
-          <div className='cat-nav-container'>
-            <div className='link-wrapper'>
+      <div className='cat-nav w-full bg-white border-b border-[#dfdfdf]'>
+        <div className='h-full w-full mx-auto flex items-center justify-center'>
+          <div className='cat-nav-container max-w-[1500px] w-full flex items-center justify-between h-full py-3 '>
+            <div className='link-wrapper h-full flex flex-1 items-center justify-between'>
               <div className='cat-nav-item'>cat 1</div>
               <div className='cat-nav-item'>cat 2</div>
               <div className='cat-nav-item'>cat 3</div>
